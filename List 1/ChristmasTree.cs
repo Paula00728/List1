54264a6a559561ce04b5251fdc306c9e849d7b66
Paula00728 <41003091+Paula00728@users.noreply.github.com>
2018-12-27 22:50:16 +0100

    Add files via upload

---
+++ List 1/ChristmasTree.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_1
+{
+    class ChristmasTree : Program
+    {      
+        public static string userInput;
+        public static int weight;
+
+        public void BasicProgram()
+        {
+            Console.WriteLine("Christmas Tree");
+            Console.WriteLine("Christmas tree weight");
+            userInput = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(userInput))
+            {
+                Console.WriteLine("You must select christmas tree weight");
+            }
+
+            else
+            {
+                weight = int.Parse(userInput);
+                PaintChristmasTree(weight);
+            }
+        }
+
+        void PaintChristmasTree(int weight)
+        {
+            int counter = 1;
+            Console.ForegroundColor = ConsoleColor.Green;
+            for (int i = 1; i <= weight; i++)
+            {    
+                {
+                    for (int j = 1; j <= weight + i - 1; j++)
+                        if (j > weight - i)
+                        {
+                            if(counter%2==0)
+                                Console.Write("*");
+                            else
+                            {
+                                Console.Write(" ");
+                            }
+                            counter++;
+                        }
+                        else
+                            Console.Write(" ");
+                    Console.WriteLine();
+                    counter = 1;
+                }
+            }
+            for (int j = 0; j < 1; j++)
+            {
+                for (int i = 0; i < weight - 1; i++)
+                {
+                    Console.Write(" ");
+                }
+            }
+
+        }
+    }
+}
+    
---
+++ List 1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace List_1
+{
+    class Program 
+    {
+        static void Main()
+        {
+            ChristmasTree christmasTree = new ChristmasTree();
+            christmasTree.BasicProgram();
+        }
+    }
+}
